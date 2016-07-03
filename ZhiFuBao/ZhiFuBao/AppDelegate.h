126fc2d91b8b601ad26771fc50f847af2da4440d
peng <xpdragon1993@163.com>
2016-07-03 13:40:30 +0800

    初始化项目

---
+++ ZhiFuBao/ZhiFuBao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZhiFuBao
+//
+//  Created by dragon on 16/7/3.
+//  Copyright © 2016年 win. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
